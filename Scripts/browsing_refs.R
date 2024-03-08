a21da0857f318916d25795204e2141df72c4f603
sraymez <santirg97@gmail.com>
2024-03-08 16:09:14 +0000

    modifying script format

--- Scripts/browsing_refs.R
+++ Scripts/browsing_refs.R
@@ -1,6 +1,10 @@
-reservoir_data_summaries <- read.csv("R/Working Data/reservoir_data_complete_coords.csv", stringsAsFactors = F)
-reservoir_refs <- read.csv("R/Working Data/reservoir_refs.csv", stringsAsFactors = F)
+## Querying references from lit review
 
+# Data
+reservoir_data_summaries <- import(here("Working Data", "reservoir_data_complete_coords.csv"))
+reservoir_refs <- import(here("Working Data", "reservoir_refs.csv"))
+
+# Find studies where lots of data was extracted
 rodents <- reservoir_data_summaries %>% 
   filter(genus %in% c("Arvicanthis", "Cricetomys", "Crocidura", "Mastomys",
                       "Mus", "Mus_nannomys", "Rattus", "Praomys", "Steatomys",
@@ -8,15 +12,16 @@ rodents <- reservoir_data_summaries %>%
   group_by(study_id) %>% 
   count()
 
-
-
+# Finding common first authors
 rodent_refs <- reservoir_refs %>% 
   filter(study_id %in% rodents$study_id) %>% 
   group_by(first_author) %>% 
   count()
 
-
+# Finding references on rift valley
 rvf <- pathogen_data_summaries %>% 
   filter(pathogen_tested == "Rift_valley_fever") %>% 
   select(study_id, year, sample_size, num_pos_pcr, num_pos_ser) %>% 
   count(study_id, year)
+
+# End 
