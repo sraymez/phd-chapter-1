e006bbe88773edf9a6cd38db608c942656a723ae
sraymez <santirg97@gmail.com>
2024-04-12 15:59:11 +0100

    adding script with raster classifications so dont need to search online each time

---
+++ Scripts/raster_classifications.R
@@ -0,0 +1,53 @@
+## Raster value classifications
+
+
+# Bioclim variables ----
+
+# BIO1 = Annual Mean Temperature
+# BIO2 = Mean Diurnal Range (Mean of monthly (max temp - min temp))
+# BIO3 = Isothermality (BIO2/BIO7) (×100)
+# BIO4 = Temperature Seasonality (standard deviation ×100)
+# BIO5 = Max Temperature of Warmest Month
+# BIO6 = Min Temperature of Coldest Month
+# BIO7 = Temperature Annual Range (BIO5-BIO6)
+# BIO8 = Mean Temperature of Wettest Quarter
+# BIO9 = Mean Temperature of Driest Quarter
+# BIO10 = Mean Temperature of Warmest Quarter
+# BIO11 = Mean Temperature of Coldest Quarter
+# BIO12 = Annual Precipitation
+# BIO13 = Precipitation of Wettest Month
+# BIO14 = Precipitation of Driest Month
+# BIO15 = Precipitation Seasonality (Coefficient of Variation)
+# BIO16 = Precipitation of Wettest Quarter
+# BIO17 = Precipitation of Driest Quarter
+# BIO18 = Precipitation of Warmest Quarter
+# BIO19 = Precipitation of Coldest Quarter
+
+
+# Land cover ----
+
+# 1 = Evergreen Needleleaf Forests
+# 2 = Evergreen Broadleaf Forests 
+# 3 = Deciduous Needleleaf Forests
+# 4 = Deciduous Broadleaf Forests 
+# 5 = Mixed Forests
+# 6 = Closed Shrublands
+# 7 = Open Shrublands
+# 8 = Woody Savanna
+# 9 = Savannas
+# 10 = Grasslands
+# 11 = Permanent Wetlands
+# 12 = Croplands
+# 13 = Urban and Built-up Lands
+# 14 = Cropland/Natural Vegetation Mosaic
+# 15 = Permanent Snow and Ice
+# 16 = Barren 
+# 17 = Water Bodies
+# 255 = Unclassified
+
+
+
+
+
+
+
